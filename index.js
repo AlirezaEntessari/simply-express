16a7ea4edb026d9b74546a323f037db56d8c7d9f
aent <alintssr@gmail.com>
2023-11-16 12:11:11 -0500

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send(`<h1>Welcome to my Express App</h1>`);
 });
 
+app.get('/patrick', (req, res) => {
+    res.send(`<h1>Welcome to my Patrick</h1>`);
+});
+
 app.listen(PORT, () => {
    console.log(`app running on port ${PORT}`)
 });
